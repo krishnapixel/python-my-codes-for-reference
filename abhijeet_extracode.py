4a1779efe10c6d521a238fc630d809133b18821c
Krishna Muthu <74429620+krishnapixel@users.noreply.github.com>
2021-09-30 19:51:22 -0400

    Add files via upload

---
+++ abhijeet_extracode.py
@@ -0,0 +1,16 @@
+#abhijeet or name
+    '''cust=[0]*(N+1)
+    for a in E:
+        if a[1]!=0:
+            if SRI[a[1]]==demand[a[1]] and cust[a[1]]==0:
+                active_arcs.append(a)
+                cust[a[1]]=1
+        else:
+            if cust[a[0]]==0 and SRI[a[0]]==demand[a[0]]:
+                active_arcs.append(a)
+                cust[a[1]]=1
+                
+            elif SRI[a[0]]==demand[a[0]] and cust[a[0]]==0:
+                active_arcs.append(a)
+                cust[a[1]]=1'''
+#end abhijeet or name
